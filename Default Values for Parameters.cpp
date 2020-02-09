22d9ddb720af578878cae2799582f399c43a58b8
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-02-09 20:26:15 +0500

    First commit

---
+++ Default Values for Parameters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b=42) {
+  int result = a + b;
+  return (result);
+}
+
+int main() {
+  int x = 24;
+  int y = 36;
+
+  //calling the function with both parameters
+  int result = sum(x, y);
+  cout << result << endl;
+  //Outputs 60
+
+  //calling the function without b
+  result = sum(x);
+  cout <<  result << endl;
+   //Outputs 66
+
+  return 0;
+}
